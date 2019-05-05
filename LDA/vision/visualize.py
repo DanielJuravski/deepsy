0930cbb81f5262a8f4057fbd1e7fcc840754842d
Daniel Juravski <juravski95@gmail.com>
2019-05-05 16:11:52 +0300

    filter by clieant name

--- LDA/vision/visualize.py
+++ LDA/vision/visualize.py
@@ -13,8 +13,7 @@ def getOptions():
         input_option_i = sys.argv.index('--input')
         file_name = sys.argv[input_option_i + 1]
     else:
-        file_name = '/home/daniel/mallet-2.0.8/inferenced_doc.txt'
-        # file_name = 'inferenced_doc.txt'
+        file_name = '../client_5/topic-composition.txt'
 
     if '--output' in sys.argv:
         output_option_i = sys.argv.index('--output')
@@ -22,12 +21,24 @@ def getOptions():
     else:
         output_name = '.'
 
+    if '--client2view' in sys.argv:
+        output_option_i = sys.argv.index('--client2view')
+        client2view_name = sys.argv[output_option_i + 1]
+        print("Visualizing only for {0}".format(client2view_name))
+    else:
+        client2view_name = None
+
     print("Visualizing: {0}".format(file_name))
 
-    return file_name, output_name
+    return file_name, output_name, client2view_name
 
 
-def process_file(file_name):
+def process_file(file_name, client2view_name):
+    '''
+    making object with the documents' topics composition
+    :param file_name:
+    :return: list, where each var is a list of doc number, doc name, doc topics.
+    '''
     composition_obj = []
     with open(file_name, 'r') as f:
         for line in f:
@@ -37,11 +48,24 @@ def process_file(file_name):
             line_details = line.split()
             # patientName_sessionNumber
             doc_name = line_details[1].split('/')[-1].split('.docx.json.parsed')[0].split('_')[0]
+
+            # extracting patient name and drop if patient name option is given
+            patientName = ''.join([i for i in doc_name if not i.isdigit()])
+            if client2view_name is not None and patientName != client2view_name:
+                continue
+
             # extracting session number and int it
             doc_number_listed = [int(s) for s in list(doc_name) if s.isdigit()]
             doc_number = int(''.join(str(e) for e in doc_number_listed))
             doc_topics = line_details[2:]
             composition_obj.append((doc_number, doc_name, doc_topics))
+
+    # check that the returned object not empty
+    if client2view_name is not None:
+        if len(composition_obj) <= 0:
+            print("There are not documents fot {0} client, returned object is empty.".format(client2view_name))
+            print("Exiting.")
+            sys.exit()
 
     composition_obj.sort(key=itemgetter(0))
 
@@ -58,16 +82,19 @@ def make_graph(composition_obj):
 
     df = DataFrame(docs_topics_dist, dtype=float, index=docs_number).T
 
+    # annot: show values over the cells
     dashboard = sns.heatmap(df, annot=False)
     dashboard.set_xticklabels(dashboard.get_xticklabels(), rotation=30)
 
     plt.show()
-
+    # plt.show(block=False)
+    # input("Hit Enter To Close")
+    # plt.close()
 
 
 if __name__ == '__main__':
-    file_name, output_name = getOptions()
-    composition_obj = process_file(file_name)
+    file_name, output_name, client2view_name = getOptions()
+    composition_obj = process_file(file_name, client2view_name)
     make_graph(composition_obj)
     
     pass
