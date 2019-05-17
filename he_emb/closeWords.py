fb9128cb61d7afc6dbab974a6013984dab383956
Daniel Juravski <juravski95@gmail.com>
2019-05-17 17:15:04 +0300

    he R2L

--- he_emb/closeWords.py
+++ he_emb/closeWords.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 
-TARGET_WORDS = ["אמא"]
+TARGET_WORDS = ["דתי"]
 
 VOCAB_FILE = '/home/daniel/Documents/he_emb/ron_shemesh/words_list.txt'
 EMB_FILE = '/home/daniel/Documents/he_emb/ron_shemesh/words_vectors.npy'
@@ -24,7 +24,7 @@ def getOptions():
 
     if '--word' in sys.argv:
         option_i = sys.argv.index('--word')
-        target_word = sys.argv[option_i + 1]
+        target_word = sys.argv[option_i + 1:]
     else:
         target_word = TARGET_WORDS
 
@@ -38,7 +38,7 @@ def getOptions():
     return vocab_file, emb_file, target_word
 
 
-def loadData(vocab_file, emb_file):
+def loadData(vocab_file, emb_file, target_word):
     print("Loading vocab")
     with open(vocab_file, 'r') as f:
         vocab = f.read().splitlines()
@@ -48,16 +48,19 @@ def loadData(vocab_file, emb_file):
     emb = np.load(emb_file)
     print("emb has been loaded\nemb length is {0}".format(len(emb)))
 
-    return vocab, emb
+    # take care if single word, then split it. for the list iteration.
+    target_word = target_word.split() if type(target_word) is not list else target_word
+
+    return vocab, emb, target_word
 
 
-def get_close_words(vocab, emb, num_of_close, remove_first=False):
+def get_close_words(vocab, emb, target_words, num_of_close, remove_first=False):
     start_index = -2 if remove_first else -1
     results = {}
     # tokenize words
     w2i = {w:i for i, w in enumerate(vocab)}
 
-    for target_word in TARGET_WORDS:
+    for target_word in target_words:
         target_v = emb[w2i[target_word]]
         dot_products = emb.dot(target_v)
         most_similar_ids = dot_products.argsort()[start_index:-(num_of_close+2):-1]
@@ -70,16 +73,16 @@ def get_close_words(vocab, emb, num_of_close, remove_first=False):
 def printThis(results):
     for target in results:
         close_words = str()
-        print("\nTarget: " + target)
-        for word in results[target]:
-            close_words += str(word) + ' '
+        print("\nTarget: " + target[::-1])
+        for word in reversed(results[target]):
+            close_words += ' ,' + str(word[::-1])
         print("Close words: " + close_words)
 
 
 if __name__ == '__main__':
     vocab_file, emb_file, target_word = getOptions()
-    vocab, emb = loadData(vocab_file, emb_file)
+    vocab, emb, target_word = loadData(vocab_file, emb_file, target_word)
 
-    results = get_close_words(vocab, emb, num_of_close=20, remove_first=False)
+    results = get_close_words(vocab, emb, target_word, num_of_close=20, remove_first=False)
 
     printThis(results)
