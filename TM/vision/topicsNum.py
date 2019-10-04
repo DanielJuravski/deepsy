2527e67b8cad873ed6924e57c475b1d6febcbbef
Daniel Juravski <juravski95@gmail.com>
2019-10-04 13:33:04 +0300

    add unique topic counting

--- TM/vision/topicsNum.py
+++ TM/vision/topicsNum.py
@@ -9,7 +9,7 @@ from copy import deepcopy
 from visualizeTopics import process_file
 
 
-TOPIC_DIST_THRESHOLD = 0.05
+TOPIC_DIST_THRESHOLD = 0.01
 
 
 def getOptions():
@@ -19,7 +19,7 @@ def getOptions():
     else:
         # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c01_sessions/results/inferencer.txt'
         # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_sessions/results/composition.txt'
-        file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_500_words/results/composition_100.txt'
+        file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/b_1000_words/results/composition_100.txt'
         # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_5turns_words/results/composition_100.txt'
 
     if '--output' in sys.argv:
@@ -42,43 +42,66 @@ def getOptions():
 
 def countTopicsAvg(doc_topics):
     dt_avg = []
+    dt_unique = []
     d_prev = d_curr = doc_topics[0][0]
     t_avg_calc = list()  #  only for calculating
+    t_unique = set()
 
     for dt in doc_topics:
         d_curr = dt[0]
-        t = dt[1]
+        t_count = dt[1]
+        t_set = dt[2]
         if d_curr != d_prev:
             avg = sum(t_avg_calc)/float(len(t_avg_calc))
             dt_avg.append((d_prev, avg))
             t_avg_calc = list()
 
-        t_avg_calc.append(int(t))
+            t_unique_count = len(t_unique)
+            dt_unique.append((d_prev, t_unique_count))
+            t_unique = set()
+
+        t_avg_calc.append(int(t_count))
+        t_unique = t_unique.union(t_set)
         d_prev = d_curr
     # handle last doc
-    avg = sum(t_avg_calc) / float(len(t_avg_calc))
-    dt_avg.append((d_prev, avg))
+    t_count_avg = sum(t_avg_calc) / float(len(t_avg_calc))
+    dt_avg.append((d_prev, t_count_avg))
+    t_unique_count = len(t_unique)
+    dt_unique.append((d_prev, t_unique_count))
 
-    return dt_avg
+    return dt_avg, dt_unique
+
+
+def boolList2Set(topics_over_threshold):
+    topics_id = set()
+    for i, val in enumerate(topics_over_threshold):
+        if val == True:
+            topics_id.add(i)
+
+    return topics_id
 
 
 def countTopics(composition_obj):
     doc_topics = []  # tuple of session number + topics number in that session
     for doc in composition_obj:
         session_number = doc[0]
-        topics_count = sum(float(prob) > TOPIC_DIST_THRESHOLD for prob in doc[2])
-        doc_topics.append((session_number, topics_count))
+        topics_count = sum(float(prob) > TOPIC_DIST_THRESHOLD for prob in doc[3])
+        topics_over_threshold = [float(prob) > TOPIC_DIST_THRESHOLD for prob in doc[3]]
+        topics_over_threshold = boolList2Set(topics_over_threshold)
+        doc_topics.append((session_number, topics_count, topics_over_threshold))
 
-    doc_topics_avg = countTopicsAvg(doc_topics)
+    doc_topics_avg, dt_unique = countTopicsAvg(doc_topics)
     print(doc_topics)
     print(doc_topics_avg)
+    print(dt_unique)
 
-    return doc_topics, doc_topics_avg
+    return doc_topics, doc_topics_avg, dt_unique
 
 
-def make_graph(doc_topics_num, doc_topics_avg):
+def make_graph(doc_topics_num, doc_topics_avg, dt_unique):
     # doc_topics_num is a list of tuples (doc-session number, topic count in that session)
     # doc_topics_avg is a list of tuples (doc-session number, avg topic count in all those sessions)
+    # dt_unique is a list of tuples (doc-session number, count unique topics is the session)
 
     # doc_topics_num process
     # make list of the topic counters
@@ -93,14 +116,23 @@ def make_graph(doc_topics_num, doc_topics_avg):
             if j[0] == session_number:
                 topic_avg_counts[i] = j[1]
 
+    # dt_unique process
+    topic_unique_counts = deepcopy(session_numbers)
+    for i, session_number in enumerate(session_numbers):
+        for j in dt_unique:
+            if j[0] == session_number:
+                topic_unique_counts[i] = j[1]
+
     plt.plot(topic_counts)
     plt.plot(topic_avg_counts)
+    plt.plot(topic_unique_counts)
 
     plt.suptitle('Session - Topics num {0}'.format(TOPIC_DIST_THRESHOLD), fontsize=20)
     plt.xlabel('Session number', fontsize=10)
     plt.ylabel('Number of Topics', fontsize=10)
     plt.xticks(np.arange(len(session_numbers)), session_numbers, rotation=90)
     plt.margins(x=0)
+    plt.gca().legend(('#Topics', 'Avg #Topics / Session', 'Unique #Topics in Session'))
     plt.grid()
     plt.show()
 
@@ -108,5 +140,5 @@ def make_graph(doc_topics_num, doc_topics_avg):
 if __name__ == '__main__':
     file_name, output_name, client2view_name = getOptions()
     composition_obj = process_file(file_name, client2view_name)
-    doc_topics_num, doc_topics_avg = countTopics(composition_obj)
-    make_graph(doc_topics_num, doc_topics_avg)
+    doc_topics_num, doc_topics_avg, dt_unique = countTopics(composition_obj)
+    make_graph(doc_topics_num, doc_topics_avg, dt_unique)
