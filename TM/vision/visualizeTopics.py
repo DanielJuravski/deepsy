16e230c32e1bfb071fe84710d77487476286fec1
Daniel Juravski <juravski95@gmail.com>
2019-10-13 12:24:09 +0300

    visualize by specific topics

--- TM/vision/visualizeTopics.py
+++ TM/vision/visualizeTopics.py
@@ -1,18 +1,21 @@
 import sys
 print(sys.version, '\n')
 import os
-
 import numpy as np
 from pandas import DataFrame
 import seaborn as sns
 import matplotlib.pyplot as plt
-from operator import itemgetter
-import re
 
-TOPICS_TO_FILTER = [0,1]
-TOPICS_TO_SAVE = []
+import re
+from pathlib import Path
+
+TOPICS_TO_SHOW = []
+TOPICS_TO_SHOW = []
+# TOPICS_TO_SAVE = []
 
 TOPIC_DIST_BASE_NAME = '_probs_dist.txt'
+METADATA_BASE_NAME = '_metadata.txt'
+
 PLT_SHOW = True
 PLT_SAVE = False
 
@@ -23,10 +26,7 @@ def getOptions():
         file_name = sys.argv[input_option_i + 1]
     else:
         # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c01_sessions/results/inferencer.txt'
-        # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_sessions/results/composition.txt'
-        # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_5turns_words/results/composition_100.txt'
-        file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/b_1000_words/results/tmp/alpha_2.5/composition_100.txt'
-        # file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_5turns_words/results/composition_100.txt'
+        file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/b_900_words/results/composition_200.txt'
 
     if '--client2view' in sys.argv:
         output_option_i = sys.argv.index('--client2view')
@@ -62,16 +62,17 @@ def ifNotExistCreate(dir_path):
         os.makedirs(dir_path)
 
 
-def filterTopics(docs_topics_dist):
-    docs_topics_dist_T = docs_topics_dist.T
-    # docs_topics_dist_T_list = list(docs_topics_dist_T)
-    for topic_number, dists in enumerate(docs_topics_dist_T):
-        if topic_number in TOPICS_TO_FILTER:
-            docs_topics_dist_T = np.delete(docs_topics_dist_T, topic_number, 0)
+def filterTopics(composition_obj):
+    for doc in composition_obj:
+        t_probs = doc[3]
+        t_probs_keys = t_probs.copy().keys()
 
-    docs_topics_dist = docs_topics_dist_T.T
+        if len(TOPICS_TO_SHOW) > 0:
+            for t in t_probs_keys:
+                if t not in TOPICS_TO_SHOW:
+                    t_probs.pop(t, None)
 
-    return docs_topics_dist
+    return composition_obj
 
 
 def process_file(file_name, client2view_name):
@@ -102,7 +103,11 @@ def process_file(file_name, client2view_name):
             # extracting session number and int it
             doc_number_listed = [int(s) for s in list(patientName_sessionNumber) if s.isdigit()]
             doc_number = int(''.join(str(e) for e in doc_number_listed))
-            doc_topics = line_details[2:]
+            doc_topics_probs = line_details[2:]
+            doc_topics = {}
+            for t, t_p in enumerate(doc_topics_probs):
+                doc_topics[t] = t_p
+
             composition_obj.append((doc_number, patientName_sessionNumber, doc_name, doc_topics))
 
     # check that the returned object not empty
@@ -119,20 +124,22 @@ def process_file(file_name, client2view_name):
             if d_object[2] == d_s:
                 composition_obj_sorted.append(d_object)
 
-    return composition_obj_sorted
+    # filter certain topics
+    composition_obj = filterTopics(composition_obj_sorted)
+
+    return composition_obj
 
 
-def make_graph(composition_obj, output_path, client2view_name):
+def makeGraph(composition_obj, output_path, client2view_name):
     print("Plotting/Saving for {}".format(client2view_name))
     docs_topics_dist_list = []
     docs_number = []
     for doc in composition_obj:
         docs_number.append(doc[0])
-        docs_topics_dist_list.append(doc[3])
+        probs_dict = doc[3]
+        docs_topics_dist_list.append([probs_dict[key] for key in sorted(probs_dict.keys())])
     docs_topics_dist = np.array(docs_topics_dist_list)
-
-    # filter certain topics
-    # docs_topics_dist_filtered = filterTopics(docs_topics_dist)
+    docs_topics_numbers_list = [str(i) for i in list(probs_dict.keys())]
 
     df = DataFrame(docs_topics_dist, dtype=float, index=docs_number).T
 
@@ -140,7 +147,15 @@ def make_graph(composition_obj, output_path, client2view_name):
     dashboard = sns.heatmap(df, annot=False)
     # just rotate the xticks labels
     dashboard.set_xticklabels(dashboard.get_xticklabels(), rotation=30)
-    dashboard.grid()
+    if len(TOPICS_TO_SHOW) > 0:
+        T = np.arange(len(docs_topics_numbers_list))+0.5
+        dashboard.set_yticks(T)
+        dashboard.set_yticklabels(docs_topics_numbers_list)
+        # dashboard.grid()
+    else:
+        dashboard.set_yticklabels(docs_topics_numbers_list, minor=True)
+        dashboard.grid()
+
     plt.suptitle('Client name: \n{0}'.format(reverse(client2view_name)), fontsize=15)
     plt.xlabel('Session number', fontsize=10)
     plt.ylabel('Topic', fontsize=10)
@@ -155,14 +170,45 @@ def make_graph(composition_obj, output_path, client2view_name):
     plt.close()
 
 
+def extractMetaData(composition_obj):
+    metadatas2write = []
+
+    for line in composition_obj:
+        file_num = line[0]
+        file_path = line[2]
+        vector = line[3:]
+
+        file_name = file_path.split('/')[-1]
+        patientName_sessionNumber = file_name.split('.docx.json.parsed')[0].split('_')[0]
+        client_name = ''.join([i for i in patientName_sessionNumber if not i.isdigit()])
+        session_number = re.sub("[^0-9]", "", patientName_sessionNumber)
+        parsed_part = re.sub("[^0-9]", "", file_name.split('.docx.json.parsed')[1])
+        metadata_str = client_name + '\t' + session_number + '\t' + parsed_part
+        metadatas2write.append(metadata_str)
+
+    return metadatas2write
+
+
 def exportTopicsDist(composition_obj, output_path, client2view_name):
-    out_str = output_path + client2view_name + TOPIC_DIST_BASE_NAME
-    with open(out_str, 'w') as f:
+    vec_f_name = output_path + client2view_name + TOPIC_DIST_BASE_NAME
+    metadata_f_name = output_path + client2view_name + METADATA_BASE_NAME
+    cleanFiles(metadata_f_name)
+
+    with open(vec_f_name, 'w') as f:
         for instance in composition_obj:
-            dist = instance[2]
-            for prob in dist:
+            dist = instance[3]
+            for (t, prob) in dist.items():
                 f.write(prob + '\t')
             f.write('\n')
+
+    metadatas2write = extractMetaData(composition_obj)
+    meta_data_first_line = 'Client_name\tSession_number\tParsed_part\n'
+    with open(metadata_f_name, 'a') as f_meta:
+        f_meta.writelines(meta_data_first_line)
+    with open(metadata_f_name, 'a') as f_meta:
+        for i in metadatas2write:
+            f_meta.writelines(i)
+            f_meta.writelines('\n')
 
 
 def reverse(s):
@@ -180,11 +226,16 @@ def sorted_alphanumeric(data):
     return sorted(data, key=alphanum_key)
 
 
+def cleanFiles(file_path):
+    if Path(file_path).exists():
+        os.remove(file_path)
+
+
 if __name__ == '__main__':
     file_name, output_path, client2view_name_list = getOptions()
 
     for client2view_name in client2view_name_list.split():
         composition_obj = process_file(file_name, client2view_name)
         exportTopicsDist(composition_obj, output_path, client2view_name)
-        make_graph(composition_obj, output_path, client2view_name)
+        makeGraph(composition_obj, output_path, client2view_name)
 
