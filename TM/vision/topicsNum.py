2325d99aecb312f88171daaa4b6045feaf223155
Daniel Juravski <juravski95@gmail.com>
2019-10-29 18:18:19 +0200

    add HTML counting feature

--- TM/vision/topicsNum.py
+++ TM/vision/topicsNum.py
@@ -6,18 +6,21 @@ import numpy as np
 from copy import deepcopy
 import yaml
 import re
+import os
 
 
-TOPIC_DIST_THRESHOLD = 0.002
+TOPIC_DIST_THRESHOLD = 0.0005
 ORS_FILE_PATH = '/home/daniel/deepsy/SBS_Analize/Trans_ORS/trans_ors.yml'
-
+WORKSPACE_PATH = '/home/daniel/deepsy/TM/Dirs_of_Docs/c_1turns_words/results_mini_small'  # change
+HTML_DIR_PATH = WORKSPACE_PATH + '/HTMLs_200'  # change
+COMPOSITION_FILE_PATH = WORKSPACE_PATH + '/composition_200.txt'  # change
 
 def getOptions():
     if '--input' in sys.argv:
         input_option_i = sys.argv.index('--input')
         file_name = sys.argv[input_option_i + 1]
     else:
-        file_name = '/home/daniel/deepsy/TM/Dirs_of_Docs/b_1000_words/results/tmp/alpha5/composition_200.txt'
+        file_name = COMPOSITION_FILE_PATH
 
     if '--output' in sys.argv:
         output_option_i = sys.argv.index('--output')
@@ -29,7 +32,7 @@ def getOptions():
         output_option_i = sys.argv.index('--client2view')
         client2view_name = sys.argv[output_option_i + 1]
     else:
-        client2view_name = 'א'
+        client2view_name = 'עה'
     
     print("Visualizing: {0}".format(file_name))
     print("Visualizing only for {0}".format(client2view_name))
@@ -155,10 +158,14 @@ def countTopics(composition_obj):
     return doc_topics, doc_topics_avg, dt_unique
 
 
-def make_graph(doc_topics_num, doc_topics_avg, dt_unique, sessions_ors, client2view_name, file_name):
+def make_graph(doc_topics_num, doc_topics_avg, dt_unique, html_t_counts, html_t_threshold_counts,
+               sessions_ors, client2view_name, file_name):
     # doc_topics_num is a list of tuples (doc-session number, topic count in that session)
     # doc_topics_avg is a list of tuples (doc-session number, avg topic count in all those sessions)
     # dt_unique is a list of tuples (doc-session number, count unique topics is the session)
+    # html_t_counts is a list of number of topics at each document by the HTML parsing script
+    # html_t_threshold_counts is a list of number of topics at each document by the HTML parsing script
+    #   (that passed the count threshold)
     # sessions_ors is a list of tuples (doc-session number, ors score of that session)
 
     # doc_topics_num process
@@ -188,10 +195,19 @@ def make_graph(doc_topics_num, doc_topics_avg, dt_unique, sessions_ors, client2v
             if j[0] == session_number:
                 sessions_ors_scores[i] = j[1]
 
+    # # html_t_counts process
+    # html_t_counts_only = deepcopy(session_numbers)
+    # for i, session_number in enumerate(session_numbers):
+    #     for j in html_t_counts:
+    #         if j[0] == session_number:
+    #             html_t_counts_only[i] = j[1]
+
     plt.plot(topic_counts)
     plt.plot(topic_avg_counts)
     plt.plot(topic_unique_counts)
     plt.plot(sessions_ors_scores)
+    plt.plot(html_t_counts)
+    plt.plot(html_t_threshold_counts)
 
     plt.suptitle('Session-Topics num {0} client:{1}'.format(TOPIC_DIST_THRESHOLD, client2view_name), fontsize=20)
     plt.title(file_name.replace('/home/daniel/deepsy/TM/Dirs_of_Docs/', ''))
@@ -199,7 +215,8 @@ def make_graph(doc_topics_num, doc_topics_avg, dt_unique, sessions_ors, client2v
     plt.ylabel('Number of Topics / ORS', fontsize=10)
     plt.xticks(np.arange(len(session_numbers)), session_numbers, rotation=90)
     plt.margins(x=0)
-    plt.gca().legend(('#Topics', 'Avg #Topics / Session', 'Unique #Topics in Session', 'ORS'))
+    plt.gca().legend(('#Topics', 'Avg #Topics / Session', 'Unique #Topics in Session', 'ORS',
+                      '#Topics (HTML)', '#Topics threshold (HTML)'))
     plt.grid()
     plt.show()
 
@@ -220,9 +237,76 @@ def getORS(client2view_name):
     return sessions_sorted_ors
 
 
+def fileName2SessionNumber(file):
+    patientName_sessionNumber = file.split('.docx.json.parsed')[0].split('_')[0]
+
+    # extracting session number and int it
+    session_number_listed = [int(s) for s in list(patientName_sessionNumber) if s.isdigit()]
+    session_number = int(''.join(str(e) for e in session_number_listed))
+
+    return session_number
+
+
+def HTMLCounts(client2view_name):
+    # Get the counts from the HTML script parsing
+
+    # Get relevant client's files
+    client_files = []
+    for file in os.listdir(HTML_DIR_PATH):
+        patientName_sessionNumber = file.split('.docx.json.parsed')[0].split('_')[0]
+
+        # extracting patient name
+        patientName = ''.join([i for i in patientName_sessionNumber if not i.isdigit()])
+        if client2view_name is not None and patientName != client2view_name:
+            continue
+
+        client_files.append(file)
+
+    # Smart sort
+    client_files = sorted_alphanumeric(client_files)
+
+    # Get counts from HTML files
+    TOPICS_STR = 'Number of topics (above threshold):'
+    t_counts = []
+    t_threshold_counts = []
+    for file in client_files:
+        file_path = HTML_DIR_PATH + '/' + file
+        session_number = fileName2SessionNumber(file)
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if TOPICS_STR in line:
+                    line_details = line.split(TOPICS_STR)  # => [Number of topics (above threshold):, number (number)]
+                    t_count = line_details[1].split('(')[0]
+                    t_threshold_count = line_details[1].split('(')[1].split(')')[0]
+
+                    # some unity checks:
+                    try:
+                        t_count = int(t_count)
+                    except TypeError:
+                        print("Can not parse HTML counts, probably something happened.")
+
+                    try:
+                        t_threshold_count = int(t_threshold_count)
+                    except TypeError:
+                        print("Can not parse HTML counts, probably something happened.")
+
+                    # t_counts.append((session_number, t_count))
+                    # t_threshold_counts.append((session_number, t_threshold_count))
+                    t_counts.append(t_count)
+                    t_threshold_counts.append(t_threshold_count)
+
+    print("HTML Topics Counts:{}".format(t_counts))
+    print("HTML Topics threshold Counts:{}".format(t_threshold_counts))
+
+    return t_counts, t_threshold_counts
+
+
 if __name__ == '__main__':
     file_name, output_name, client2view_name = getOptions()
     composition_obj = process_file(file_name, client2view_name)
     doc_topics_num, doc_topics_avg, dt_unique = countTopics(composition_obj)
     sessions_ors = getORS(client2view_name)
-    make_graph(doc_topics_num, doc_topics_avg, dt_unique, sessions_ors, client2view_name, file_name)
+    html_t_counts, html_t_threshold_counts = HTMLCounts(client2view_name)
+    make_graph(doc_topics_num, doc_topics_avg, dt_unique, html_t_counts, html_t_threshold_counts,
+               sessions_ors, client2view_name, file_name)
